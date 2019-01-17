46e75287f26ef3e82a6a5797a7265d3b41432cf1
DK\obr <olegbraginskyy@sitecore.net>
2019-01-17 09:33:58 +0200

    Add try/catch to prevent the NRE when the item is null (item was deleted)

---
+++ src/Sitecore.Support.304700/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.304700")]
+[assembly: AssemblyProduct("Sitecore.Support.304700")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.304700/SxaItemCrawler.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.ContentSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Support.XA.Foundation.VersionSpecific.Search
+{
+  public class SxaItemCrawler: Sitecore.XA.Foundation.VersionSpecific.Search.SxaItemCrawler
+  {   
+
+    protected override bool IsExcludedFromIndex(IIndexableUniqueId indexableUniqueId, IndexEntryOperationContext operationContext, bool checkLocation)
+    {
+      try
+      {
+        return base.IsExcludedFromIndex(indexableUniqueId, operationContext, checkLocation);
+      }
+      catch
+      {
+        return false;
+      }
+    }
+  }
+}
